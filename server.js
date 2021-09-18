adc1fc0fde40135dc21d8c88d796b723d0b9c710
kamm8899 <kamm8899@gmail.com>
2021-09-18 06:41:28 -0700

    added new api routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
+const app = express();
+const PORT = process.env.PORT || 3002;
+
 //install Handlebars.js 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -9,14 +12,11 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-const app = express();
-const PORT = process.env.PORT || 3002;
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
