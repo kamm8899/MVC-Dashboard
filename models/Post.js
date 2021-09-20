a07c4cb9acb42805c888d342be6a0127dede09fd
kamm8899 <kamm8899@gmail.com>
2021-09-20 14:18:26 -0700

    updated models, removed vote and every mention of this model

--- models/Post.js
+++ models/Post.js
@@ -1,41 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { User, Vote, Comment } = require('./index.js');
+// const { User,Comment } = require('./index.js');
 
 
 // create our Post model
-class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-          {
-            model: models.Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: models.User,
-              attributes: ['username']
-            }
-          }
-        ]
-      });
-    });
-  }
-}
-
+class Post extends Model {}
 // create fields/columns for Post model
 Post.init(
   {
@@ -49,12 +18,9 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    post_url: {
-      type: DataTypes.STRING,
+    post_content: {
+      type: DataTypes.TEXT,
       allowNull: false,
-      validate: {
-        isURL: true
-      }
     },
     user_id: {
       type: DataTypes.INTEGER,
