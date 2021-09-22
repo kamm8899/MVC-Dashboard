04ac7d2098da8d94ba23a34061706e11bed36b5f
kamm8899 <kamm8899@gmail.com>
2021-09-21 19:27:23 -0700

    added comment styling, and debigged login and signup page

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,26 +1,32 @@
+// Sign up form handler
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
+  event.preventDefault();
+
+  console.log(document.querySelector('#username-signup').value.trim());
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+ 
+  if (username && email && password) {
+      
       const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
+          method: 'post',
+          body: JSON.stringify({
+              username,
+              email,
+              password
+          }),
+          headers: {'Content-Type': 'application/json'}
       });
-  
+      
       if (response.ok) {
-        document.location.replace('/dashboard/');
+          alert('Signup sucessful');
+          document.location.replace('/dashboard');
       } else {
-        alert(response.statusText);
+          alert(response.statusText)
       }
-    }
   }
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
