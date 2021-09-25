eddd132d44ddb909bee2ca2cd2f68c62e8f4f450
kamm8899 <kamm8899@gmail.com>
2021-09-25 07:47:44 -0700

    debugging edit route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -42,47 +42,47 @@ router.get('/', withAuth, (req, res) =>{
 });
 
 router.get('/edit/:id', withAuth, (res, req) =>{
-    Post.findOne({
-      where:{
-      id: req.params.id
-      }, 
-        attributes: [
-          'id',
-          'title',
-          'post_content',
-          'created_at'
-        ],
-        include: [
-          {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
+  
+    Post.findByPk(req.params.id,{
+      //   attributes: [
+      //     'id',
+      //     'title',
+      //     'post_content',
+      //     'created_at'
+      //   ],
+      //   include: [
+      //     {
+      //       model: Comment,
+      //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      //       include: {
+      //         model: User,
+      //         attributes: ['username']
+      //       }
+      //     },
+      //     {
+      //       model: User,
+      //       attributes: ['username']
+      //     }
+      //   ]
+       })
         .then(dbPostData => {
           if (dbPostData) {
-            res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-            const post = dbPostData.get({plain: true });
+            const post = dbPostData.get({ plain: true });
             
             res.render('edit-post', {post,loggedIn: true});
-          })
+          } else {
+            res.status(404).end();
+          }
+        })
         .catch(err => {
           res.status(500).json(err);
         });
     });
+    
 
     //add create route for post
     router.get('/create', withAuth, (req,res) =>{
+      console.log()
       Post.findAll({
         where:{
           user_id: req.session.user_id
